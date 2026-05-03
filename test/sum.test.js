437ec583d6db1092b0568ad324ec6e19e2a81c1f
Peter Chege <roychegeh@gmail.com>
2026-05-03 17:53:11 +0300

    Initial commit

---
+++ test/sum.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import { sum } from "../src/sum";
+
+describe("sum", () => {
+    it("adds two numbers", () => {
+        expect(sum(2,3)).toBe(5)
+    })
+})
